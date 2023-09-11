9a192296e60a38fc937ae066c2381d8e9c25d7c8
doceazedo <me@doceazedo.com>
2023-09-11 12:51:15 -0300

    Implement industries store

---
+++ src/lib/stores/industries.ts
@@ -0,0 +1,32 @@
+import { writable } from 'svelte/store';
+
+type IndustriesResponse = {
+	id: number;
+	name: string;
+	type: string;
+	synonyms: string[];
+	services: Service[];
+	wpId: number;
+	isPopular: boolean | null;
+	mostCommonServices: number[];
+}[];
+
+type Service = {
+	id: number;
+	name: string;
+};
+
+const getIndustries = async () => {
+	try {
+		const resp = await fetch('https://dev-api.voolt.com/api/cms/industries');
+		if (!resp.ok) throw Error(`${resp.status} ${resp.statusText}`);
+		const data = await resp.json();
+		INDUSTRIES.set(data);
+	} catch (error) {
+		// TODO: handle errors
+	}
+};
+
+export const INDUSTRIES = writable<IndustriesResponse>(undefined, () => {
+	getIndustries();
+});
