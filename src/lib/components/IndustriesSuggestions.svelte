a917acd5118786cad08aaaf1511391d394658ea0
doceazedo <me@doceazedo.com>
2023-09-11 16:55:56 -0300

    Add transition to IndustriesSuggestions

--- src/lib/components/IndustriesSuggestions.svelte
+++ src/lib/components/IndustriesSuggestions.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 	import { INDUSTRIES, SELECTED_INDUSTRY } from '$lib/stores/industries';
 
 	$: popularOptions = $INDUSTRIES?.filter((x) => x.isPopular) || [];
@@ -6,18 +8,22 @@
 	const pickIndustry = (id: number) => ($SELECTED_INDUSTRY = id);
 </script>
 
-<h2 class="title">Or select from the most popular:</h2>
-<div class="options">
-	{#each popularOptions as option}
-		<button
-			class="option"
-			on:click={() => pickIndustry(option.id)}
-			class:is-active={option.id == $SELECTED_INDUSTRY}
-		>
-			{option.name}
-		</button>
-	{/each}
-</div>
+{#if popularOptions.length}
+	<div class="suggestions" transition:slide={{ duration: 400, easing: quintOut }}>
+		<h2 class="title">Or select from the most popular:</h2>
+		<div class="options">
+			{#each popularOptions as option}
+				<button
+					class="option"
+					on:click={() => pickIndustry(option.id)}
+					class:is-active={option.id == $SELECTED_INDUSTRY}
+				>
+					{option.name}
+				</button>
+			{/each}
+		</div>
+	</div>
+{/if}
 
 <style>
 	.title {
