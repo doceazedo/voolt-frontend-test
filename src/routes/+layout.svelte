2590374d46a378400e2089927d4f9a8c23a2a12d
doceazedo <me@doceazedo.com>
2023-09-11 11:36:12 -0300

    Setup base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,42 @@
 	import '$lib/css/global.css';
 </script>
 
-<slot />
+<div class="section">
+	<aside class="highlight" />
+	<main class="main">
+		<div class="content">
+			<slot />
+		</div>
+	</main>
+</div>
+
+<style>
+	.section {
+		display: flex;
+		height: 100svh;
+	}
+
+	.highlight {
+		max-width: 52.5rem;
+		width: 100%;
+		height: 100%;
+		flex-shrink: 0;
+		background-image: url('/images/app-demo.webp');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+
+	.main {
+		display: flex;
+		justify-content: center;
+		flex-grow: 1;
+	}
+
+	.main .content {
+		max-width: 40rem;
+		width: 100%;
+		height: 100%;
+		padding: 2.25rem 0 5rem;
+	}
+</style>
